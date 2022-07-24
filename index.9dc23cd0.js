3749ec2e0f02dcd2e858a104b8affe7980c067fe
Lluitiy <106203735+Lluitiy@users.noreply.github.com>
2022-07-24 18:23:04 +0000

    Deploying to gh-pages from @ Lluitiy/fifth-element@242d463499f08f48e4e028847ac8faa0f464d27a 🚀

---
+++ index.9dc23cd0.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[order-button]"),t=document.querySelector("[order-menu]");e.addEventListener("click",(function(){var r="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("order-open"),e.setAttribute("aria-expanded",!r),t.classList.toggle("order-open")}))}();
+//# sourceMappingURL=index.9dc23cd0.js.map
