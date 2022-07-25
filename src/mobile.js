2c2042ce054114d1bc0f855c6cacf994fc897d14
Veliiev-Renat <107556403+Veliiev-Renat@users.noreply.github.com>
2022-07-25 20:11:11 +0300

    All

--- src/mobile.js
+++ src/mobile.js
@@ -6,10 +6,55 @@
     btn.addEventListener('click', () => {
       const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
 
-      btn.classList.toggle('is-open');
+      btn.classList.toggle('-');
       btn.setAttribute('aria-expanded', !expanded);
 
       mobileMenuRef.classList.toggle('is-open');
+    })
+  );
+})();
+(() => {
+  const menuBtnRef = document.querySelectorAll('[data-menu-button-1]');
+  const mobileMenuRef = document.querySelector('[data-menu-1]');
+
+  menuBtnRef.forEach(btn =>
+    btn.addEventListener('click', () => {
+      const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+
+      btn.classList.toggle('hop');
+      btn.setAttribute('aria-expanded', !expanded);
+
+      mobileMenuRef.classList.toggle('is-open-1');
+    })
+  );
+})();
+(() => {
+  const menuBtnRef = document.querySelectorAll('[data-menu-button-2]');
+  const mobileMenuRef = document.querySelector('[data-menu-2]');
+
+  menuBtnRef.forEach(btn =>
+    btn.addEventListener('click', () => {
+      const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+
+      btn.classList.toggle('hop');
+      btn.setAttribute('aria-expanded', !expanded);
+
+      mobileMenuRef.classList.toggle('is-open-2');
+    })
+  );
+})();
+(() => {
+  const menuBtnRef = document.querySelectorAll('[data-menu-button-3]');
+  const mobileMenuRef = document.querySelector('[data-menu-3]');
+
+  menuBtnRef.forEach(btn =>
+    btn.addEventListener('click', () => {
+      const expanded = btn.getAttribute('aria-expanded') === 'true' || false;
+
+      btn.classList.toggle('hop');
+      btn.setAttribute('aria-expanded', !expanded);
+
+      mobileMenuRef.classList.toggle('is-open-3');
     })
   );
 })();
